7b4e99e3b267199cecc567b994834b5e89019abf
Victor Chen <victor.chen11@education.nsw.gov.au>
2025-11-24 12:24:41 +1100

    workinh on sqlite3

---
+++ database/queries.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE Students(id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                        firstname TEXT NOT NULL,
+--                        lastname TEXT NOT NULL,
+--                        dob TEXT NOT NULL);
+--CREATE TABLE Marks(id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                        student_id INTEGER,
+--                        subject TEXT NOT NULL,
+--                        mark INTEGER);
+INSERT INTO Students(firstname, lastname, dob)
+            VALUES('Jim', 'Smith', '26.09.2007');
+
