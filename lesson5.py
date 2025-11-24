148ee8019e4fe5b508a4a676568c5024e136e77d
Victor Chen <victor.chen11@education.nsw.gov.au>
2025-11-24 11:51:45 +1100

    finished lesson 5

--- lesson5.py
+++ lesson5.py
@@ -9,13 +9,13 @@ def home():
         <html>
         <head>
             <title>My styled page</title>
-            <link rel="stylesheet" type="text/css" href="/FLASK-REPOS/lesson5.py/static/css/style.css">
+            <link rel="stylesheet" type="text/css" href="static/css/style.css">
         </head>
         <body>
             <h1>Welcome to my stylish page</h2>
             <p>this should look nice</p>
             <p>all made with flask</p>
-            <img src="/FLASK-REPOS/lesson5.py/static/images/Flask_logo.svg" alt="Flask logo">
+            <img src="static/images/Flask_logo.svg" alt="Flask logo">
         </body>
         </html>
     """
